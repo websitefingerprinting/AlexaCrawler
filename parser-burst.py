1971dca993dc2f599b0f05aba872eeec3d3d6219
龚佳俊 <jgongac@connect.ust.hk>
2019-12-19 14:21:22 +0800

    parser that deals with n-in-1 packets

---
+++ parser-burst.py
@@ -0,0 +1,128 @@
+import multiprocessing as mp 
+import logging
+import os
+from os import makedirs
+from os.path import join, abspath, dirname, pardir
+import numpy as np
+import subprocess
+import argparse
+from scapy.all import *
+import glob
+
+src = '10.0.0.4'
+dst = '13.75.95.89'
+isDummy = 888
+isReal = 1
+pktSize = 612
+#header 3 bytes
+cellSize = 543 + 3 
+ParsedDir = join(abspath(join(dirname(__file__), pardir)) , "AlexaCrawler/parsed")
+
+def init_directories(path):
+	# Create a results dir if it doesn't exist yet
+	if not os.path.exists(path):
+		makedirs(path)
+
+
+def getTimestamp(pkt, t0):
+	return float(pkt.time - t0)
+
+def getPktType(pkt):
+	return isDummy if pkt.load[0] else isReal
+
+def getDirection(pkt):
+	if pkt.payload.src == src:
+		return 1
+	elif pkt.payload.src == dst:
+		return -1 
+	else:
+		raise ValueError("Wrong IP address!")
+
+
+def parse_arguments():
+
+	parser = argparse.ArgumentParser(description='Parse captured traffic.')
+
+	parser.add_argument('dir',
+						type=str,
+						metavar='<dataset path>',
+						help='Path of dataset.')
+	parser.add_argument('-suffix',
+						type=str,
+						metavar='<parsed file suffix>',
+						default='.cell',
+						help='to save file as xx.suffix')
+
+	# Parse arguments
+	args = parser.parse_args()
+	return args
+
+
+def parse(fdir):
+	global savedir, suffix
+	savefiledir = join(savedir, fdir.split('/')[-1].split('.pcap')[0]+suffix) 
+	packets = rdpcap(fdir)
+	print(savefiledir)
+	cnt = {1:0,-1:0}
+	with open(savefiledir, 'w') as f:
+		for i, pkt in enumerate(packets):
+			#skip the first few noise packets
+			if  getDirection(pkt)>0 and ( getPktType(pkt) == isReal ) :
+				start = i
+				t0 = pkt.time
+				print("Start from pkt no. {}".format(start))
+				break
+
+		for i,pkt in enumerate(packets[start:]):
+			#retransmission
+			if len(pkt) < pktSize:
+				continue
+			elif len(pkt) == pktSize:
+				#one cell 
+				timestamp = getTimestamp(pkt,t0)
+				pkttype = getPktType(pkt)
+				direction = getDirection(pkt)
+				cnt[direction] += 1
+				f.write( "{:.4f}\t{:d}\n".format(timestamp, pkttype * direction))					
+			else:
+			    #len(pkt) > pktSize
+				if getDirection(pkt) > 0 :
+					#This can be very rare to happen, several outgoing packets got retransmitted
+					#Treat them as all real packets
+					num_pkt = np.math.ceil( len(pkt)/pktSize )
+					timestamp = getTimestamp(pkt,t0)
+					direction = getDirection(pkt)
+					cnt[direction] += 1
+					for _ in range(num_pkt):
+						f.write("{:.4f}\t{:d}\n".format(timestamp, isReal * direction))
+				else:
+					#multiple incoming cells
+					payload = pkt.load
+					timestamp = getTimestamp(pkt,t0)
+					for b in range(0, len(payload), cellSize):
+						pkttype = isDummy if payload[b] else isReal
+						cnt[-1] += 1
+						f.write("{:.4f}\t{:d}\n".format(timestamp, pkttype * (-1))) 
+
+				
+
+	if cnt[1] < 5 and cnt[-1]< 5:
+		print("{} has too few packets:+{},-{}".format(savefiledir, cnt[1],cnt[-1]))
+
+if __name__ == "__main__":
+	global savedir, suffix
+	args = parse_arguments()
+	suffix = args.suffix
+	filelist = glob.glob(join(args.dir, '*.pcap'))
+	filename = args.dir.split("/")[-2]
+	savedir = join(ParsedDir, filename)
+	init_directories(savedir)
+
+	# for f in filelist:
+	# 	parse(f)
+
+	pool = mp.Pool(processes=15)
+	pool.map(parse, filelist)
+
+
+
