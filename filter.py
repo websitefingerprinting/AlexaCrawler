15cf8f715d5c6fb06ce31456790d2e8b51317e41
龚佳俊 <jgongac@connect.ust.hk>
2020-01-18 14:48:50 +0800

    filter

---
+++ filter.py
@@ -0,0 +1,46 @@
+import multiprocessing as mp 
+import logging
+import os
+from os.path import join
+import numpy as np
+import subprocess
+import argparse
+from scapy.all import *
+import glob
+import subprocess
+
+
+def parse_arguments():
+
+	parser = argparse.ArgumentParser(description='Remove retransmission and ACKS')
+
+	parser.add_argument('dir',
+						type=str,
+						metavar='<dataset path>',
+						help='Path of dataset.')
+
+	# Parse arguments
+	args = parser.parse_args()
+	return args
+
+
+def clean(fdir):
+	global pardir
+	tmpname = fdir.split("/")[-1]
+	tmpname  = "filter_"+tmpname
+	tmpdir = join(pardir, tmpname)
+	cmd = 'tshark -r '+ fdir +' -Y "not(tcp.analysis.retransmission or tcp.len == 0 )" -w ' + tmpdir
+	subprocess.call(cmd, shell= True)
+	subprocess.call("rm -f "+fdir,shell =True)
+	subprocess.call("mv "+tmpdir+" "+fdir,shell=True)
+
+if __name__ == "__main__":
+	global pardir 
+	args = parse_arguments()
+	pardir = args.dir
+	filelist = glob.glob(join(args.dir, '*.pcap'))
+	pool = mp.Pool(processes=15)
+	pool.map(clean, filelist)
+
+
+
