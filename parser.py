c2c75d80f1bc7db4a094f836ac6e1a3fd70e9150
龚佳俊 <jgongac@connect.ust.hk>
2020-04-28 19:28:43 +0800

    cut off tail

--- parser.py
+++ parser.py
@@ -211,7 +211,7 @@ def burst_parse(fdir):
             print("{}: cut off at {}/{}".format(id_, cut_off_ind,len(total_pkts0)))
             
         with open(savefiledir, 'w') as f:
-            for pkt in total_pkts0:
+            for pkt in total_pkts0[:cut_off_ind]:
                 f.write("{:.6f}\t{:.0f}\n".format(pkt[0],pkt[1])) 
     except Exception as e: 
         print("Error in {}: {}".format(fdir.split('/')[-1], e))
