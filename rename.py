e9a64fd6b8b575cfd0095f6576878364569544f6
龚佳俊 <jgongac@connect.ust.hk>
2020-05-29 11:14:45 +0800

    updates

--- rename.py
+++ rename.py
@@ -7,7 +7,7 @@ import re
 import argparse
 import time
 
-MON_SITE_NUM = 100
+# MON_SITE_NUM = 100
 def parse_arguments():
 
     parser = argparse.ArgumentParser(description='Crawl Alexa top websites and capture the traffic')
@@ -16,14 +16,20 @@ def parse_arguments():
                         type=str,
                         metavar='<batch dir>',
                         help='bacth folders')
-    parser.add_argument('-m',
+    parser.add_argument('-u',
                         action='store_true', 
                         default=False,
-                        help='The type of dataset: is mon or unmon?.')
-    parser.add_argument('-n0',
-                        type = int,
+                        help='The type of dataset: is mon or unmon? (default: False).')
+    parser.add_argument('-start',
+                        type=int,
+                        metavar='<start ind>',
                         default=0,
-                        help='initial index.')
+                        help='Start from which site in the list (include this ind).')
+    parser.add_argument('-end',
+                        type=int,
+                        metavar='<end ind>',
+                        default=50,
+                        help='End to which site in the list (exclude this ind).')
     parser.add_argument('-format',
                         type=str,
                         metavar='<file suffix>',
@@ -40,9 +46,9 @@ if __name__ == '__main__':
     l = []
     cnt = 0
     flist = []
-    if args.m:
+    if not args.u:
         #mon page
-        for i in range(MON_SITE_NUM):
+        for i in range(args.start, args.end):
             flist_cls = glob.glob(join(args.dir, str(i) +  '-' + '*'+args.format))
             if len(flist_cls) > 0:
                 l.append(i)
@@ -51,7 +57,7 @@ if __name__ == '__main__':
         print("Total {} webs".format(cnt))
         assert len(l) == len(flist)
         for new_index, old_label in enumerate(l):
-            new_label  = new_index + args.n0
+            new_label  = new_index + args.start
             if new_label == old_label:
                 continue
             flist_cls = flist[new_index]
@@ -63,6 +69,17 @@ if __name__ == '__main__':
                 cmd = "mv " + f + " " + new_f
                 subprocess.call(cmd, shell = True)
 
-
-
+    else:
+        for i in range(args.start, args.end):
+            fname = join(args.dir, str(i) + args.format)
+            if os.path.exists(fname):
+                flist.append( fname )
+                cnt += 1
+        print("Total {} webs".format(cnt))
+        for new_index, f in enumerate(flist):
+            new_f = join(args.dir, str(new_index) + args.format )
+            if new_f == f:
+                continue
+            cmd = "mv " + f + " " + new_f
+            subprocess.call(cmd, shell = True)
 
