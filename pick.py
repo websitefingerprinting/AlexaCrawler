78900b1ee4d854df2ec3b6f727252d6b97b050e5
龚佳俊 <jgongac@connect.ust.hk>
2020-08-16 14:43:21 +0800

    change pick behavior

--- pick.py
+++ pick.py
@@ -11,9 +11,9 @@ import pandas as pd
 
 ParsedDir = join(abspath(join(dirname(__file__), pardir)) , "AlexaCrawler/parsed")
 def init_directories(outputdir):
-    # Create a results dir if it doesn't exist yet
-    if not os.path.exists(outputdir):
-        makedirs(outputdir)
+	# Create a results dir if it doesn't exist yet
+	if not os.path.exists(outputdir):
+		makedirs(outputdir)
 
 
 
@@ -29,24 +29,19 @@ def parse_arguments():
 						type=int,
 						default=100,
 						metavar='<Num of examples wanted>',
-						help='Num of examples wanted out of all.')	
+						help='Num of examples wanted out of all.')
 	parser.add_argument('-suffix',
 						type=str,
 						metavar='<parsed file suffix>',
 						default='.cell',
 						help='to save file as xx.suffix')
-	parser.add_argument('-mode',
-						type=str,
-						metavar='<mode: mon, unmon>',
-						default='mon',
-						help='Pick in monitored or unmonitored websites')
 	# Parse arguments
 	args = parser.parse_args()
 	return args
 
 
 def analyse(fdir):
-	with open(fdir,'r') as f:
+	with open(fdir , 'r') as f:
 		l = len(f.readlines())
 	name = fdir.split("/")[-1]
 	if '-' in name:
@@ -54,7 +49,7 @@ def analyse(fdir):
 	else:
 		label = -1
 	return [label, fdir, l]
-	
+
 if __name__ == "__main__":
 	args = parse_arguments()
 	filelist = glob.glob(join(args.dir, '*'+args.suffix))
@@ -65,29 +60,26 @@ if __name__ == "__main__":
 	info = pd.DataFrame(info,columns = ['label','fdir','l'])
 	maxlabel = info.label.max()+1
 	print("max",maxlabel)
-	info.sort_values(['label','l'], ascending = [True,False],inplace=True)
-	res = info.groupby('label').head(args.m).fdir
-	if args.mode == 'mon':
-		counter = [0]*maxlabel
-	else:
-		counter = [0]
-
-	for f in res:
-		oldname = f.split("/")[-1].split(args.suffix)[0]
-		if "-" in oldname:
+	info.sort_values(['label','l'], ascending = [True,True],inplace=True)
+	grouped = info.groupby('label')
+	counter = [0]*maxlabel
+	for name, group in grouped:
+		length = len(group)
+		if length > args.m:
+			selected = group.iloc[length//2+args.m//2 - args.m :length//2+args.m//2,:].fdir
+			print("[{},{}] out of [0,{}]".format(length//2+args.m//2 - args.m, length//2+args.m//2,length))
+		else:
+			selected = group.fdir
+		for f in selected:
+			oldname = f.split("/")[-1].split(args.suffix)[0]
 			#mon
 			label = int(oldname.split("-")[0])
 			instnum = counter[label]
 			counter[label] += 1
 			newname = str(label) + "-" + str(instnum) + args.suffix
 			cmd = "cp " + f + " " + join(outputdir, newname)
-		else:
-			#unmon
-			label = counter[0]
-			newname = str(label)+args.suffix
-			counter[0] += 1
-			cmd = "cp " + f + " " + join(outputdir, newname)
-		subprocess.call(cmd, shell= True)
+
+			subprocess.call(cmd, shell= True)
 
 
 
