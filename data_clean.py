28b359d6fb5f828330fb06e1c19e453b0d306edc
Jiawen Zhang <zhangjiawen281x@gmail.com>
2021-03-19 23:40:49 -0700

    Update 24500-25500 bad sites

--- data_clean.py
+++ data_clean.py
@@ -4,7 +4,7 @@ import time
 removeList = []
 
 # Process the badlist to extract the entry num of bad link
-with open('dump/clean_0318_032939/bad.list', 'r') as badList:
+with open('dump/clean_0319_014446/bad.list', 'r') as badList:
     for line in badList:
         elements = line.split('/')
         entryNum = elements[-1].split('-')
