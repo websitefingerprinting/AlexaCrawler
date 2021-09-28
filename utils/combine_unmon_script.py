e936689a5b377ad065c570d895de4fbd39c9a483
Jiajun GONG <jgongac@connect.ust.hk>
2021-09-28 15:58:21 +0800

    update script

--- utils/combine_unmon_script.py
+++ utils/combine_unmon_script.py
@@ -1,12 +1,14 @@
 import subprocess
 import os
 from os.path import join
+import numpy as np
 
 undefended = [
     'udataset0_14000_0909_131435',
     'udataset14000_28000_0913_140751',
     'udataset28000_41000_0917_134236',
     'udataset41000_53000_0921_113335',
+    'udataset53000_60000_0924_220701',
 ]
 
 tamaraw = [
@@ -14,6 +16,7 @@ tamaraw = [
     'udataset14000_28000_0914_164409',
     'udataset28000_41000_0918_133455',
     'udataset41000_53000_0922_153343',
+    'udataset53000_60000_0925_110045',
 ]
 
 front = [
@@ -21,6 +24,7 @@ front = [
     'udataset14000_28000_0915_171955',
     'udataset28000_41000_0919_170332',
     'udataset41000_53000_0924_110452',
+    'udataset53000_60000_0926_111457',
 ]
 
 wfgan = [
@@ -28,12 +32,13 @@ wfgan = [
     'udataset14000_28000_0916_170452',
     'udataset28000_41000_0920_151238',
     'udataset41000_53000_0924_111719',
+    'udataset53000_60000_0926_232930',
 ]
 
 par_dir = '/ssd/jgongac/AlexaCrawler/parsed'
 output_dir = '/ssd/jgongac/AlexaCrawler/parsed'
 start = 0
-end = 53000
+end = 60000
 
 
 def combine_script(flist):
@@ -61,8 +66,11 @@ tamaraw_abs = check_list(tamaraw)
 front_abs = check_list(front)
 wfgan_abs = check_list(wfgan)
 
+# make sure did not mistakenly use the same folder for some defenses
+assert len(np.unique(undefended + tamaraw + front + wfgan)) == len(undefended) + len(tamaraw) + len(front) + len(wfgan)
+
 # combine
-# combine_script(undefended_abs)
+combine_script(undefended_abs)
 combine_script(tamaraw_abs)
 combine_script(front_abs)
 combine_script(wfgan_abs)
---
+++ utils/wfgan_eval_ovhd_script.sh
@@ -0,0 +1,8 @@
+# tamaraw 
+python3 ovhd_calc.py --o_mon ./parsed/dataset0_100_0826_225925/  --o_unmon ./parsed/udataset0_60000_0927_104454/ --p_mon ./parsed/dataset0_100_0827_135513/ --p_unmon ./parsed/udataset0_60000_0927_104857/ 
+
+# front 
+python3 ovhd_calc.py --o_mon ./parsed/dataset0_100_0826_225925/  --o_unmon ./parsed/udataset0_60000_0927_104454/ --p_mon ./parsed/dataset0_100_0828_103241/ --p_unmon ./parsed/udataset0_60000_0927_105321/
+
+# wfgan 
+python3 ovhd_calc.py --o_mon ./parsed/dataset0_100_0826_225925/  --o_unmon ./parsed/udataset0_60000_0927_104454/ --p_mon ./parsed/dataset0_100_0831_142555/ --p_unmon ./parsed/udataset0_60000_0927_105731/
